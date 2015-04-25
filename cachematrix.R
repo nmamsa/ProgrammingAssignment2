8cd38b1ba2622446d1d31f23c8f58b77826a3b83
nmamsa <z001cmr@1NJZ7R1.dist.target.com>
2015-04-25 23:14:04 +0530

    strore inverse
    Course era assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Assignment wk2
+# y<-c(7,1,3,4)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
+
+# a<-makeCacheMatrix(matrix(1:4,nrow=2,ncol=2))
+# 
+# cacheSolve(a)
+# a$set(matrix(5:8,nrow=2,ncol=2))
+
+
+# a$getinverse()
+# a$setinverse()
+# a$set(c(7,1,3,4))
+
